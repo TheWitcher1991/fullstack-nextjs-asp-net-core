923ea6551e20e56c153d6064ffd6bf61e3890ff6
Ashot Svazyan <ashot.svazyan222@gmail.com>
2025-02-07 15:35:41 +0300

    init

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	compress: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	generateEtags: true,
+	poweredByHeader: false,
+	productionBrowserSourceMaps: false,
+	crossOrigin: 'use-credentials',
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		remotePatterns: [],
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://localhost:8000/api/:path*',
+			},
+		]
+	},
+}
 
-export default nextConfig;
+export default nextConfig
---
+++ frontend/src/@types/declaration.d.ts
@@ -0,0 +1,114 @@
+import React from 'react'
+
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+
+declare global {
+	type EmptyObject = Record<string, never>
+
+	type Nullable<T> = T | null
+
+	type Size = 'sm' | 'md' | 'lg'
+
+	type ImageData =
+		| import('next/dist/shared/lib/get-img-props').StaticImport
+		| string
+
+	type ButtonVariant =
+		| 'default'
+		| 'primary'
+		| 'secondary'
+		| 'black'
+		| 'danger'
+		| 'overlay'
+
+	type MenuItem = {
+		name: string
+		ico: ImageData
+		href: string
+		variant?: 'active' | 'disabled' | 'default'
+		actions?: Partial<MenuItem>[]
+		disabled?: boolean
+	}
+
+	type ChipVariant = 'default' | 'primary' | 'success' | 'warning' | 'danger'
+
+	export interface ListResponse<T> {
+		meta: {
+			current_page: number
+			total_rows: number
+			total_pages: number
+		}
+		response: T[]
+	}
+
+	interface ValidationErrorResponse<
+		T extends Record<string, any> = Record<string, any>,
+	> {
+		detail:
+			| Array<{
+					type: string
+					loc: keyof T[] | string[]
+					msg: string
+					input: string
+					ctx: Record<string, string | number>
+			  }>
+			| string
+	}
+
+	interface PagesListResponse<T> {
+		pages: ListResponse<T>[]
+		pageParams: number[]
+	}
+
+	interface SocketIOMessage<IOM = string, IOA = string> {
+		message: IOM
+		action: IOA
+	}
+
+	interface ModalComponentProps {
+		isOpen: boolean
+		onClose: () => void
+		minimal?: boolean
+	}
+
+	type SelectOption = {
+		label: string
+		value?: string
+		disabled?: boolean
+		selected?: boolean
+		hr?: boolean
+	}
+
+	type OmitClassName<T> = Omit<T, 'className'>
+
+	type DetailedDivProps = React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLDivElement>,
+		HTMLDivElement
+	>
+
+	type DetailedButtonProps = React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	>
+
+	type DetailedInputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+	type DetailedSelectProps = React.DetailedHTMLProps<
+		React.SelectHTMLAttributes<HTMLSelectElement>,
+		HTMLSelectElement
+	>
+
+	type DetailedLabelProps = React.DetailedHTMLProps<
+		React.LabelHTMLAttributes<HTMLLabelElement>,
+		HTMLLabelElement
+	>
+
+	type DetailedTextareaProps = React.DetailedHTMLProps<
+		React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+		HTMLTextAreaElement
+	>
+}
+
+export {}
