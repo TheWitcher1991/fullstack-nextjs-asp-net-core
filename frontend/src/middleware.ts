fda00e927099c595ee8f43a7b673b899a60e9031
Ashot Svazyan <ashot.svazyan222@gmail.com>
2025-03-20 19:57:59 +0300

    update middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,7 +1,27 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+import links from '~packages/links'
+import { ACCESS_TOKEN_NAME } from '~packages/system'
+
 export default function middleware(req: NextRequest) {
+	const redirect = (str: string) =>
+		NextResponse.redirect(new URL(str, req.url))
+
+	const isAuth = req.cookies.has(ACCESS_TOKEN_NAME)
+
+	const guestLinks = [links.login, links.register]
+
+	const isGuestLinks = guestLinks.includes(req.nextUrl.pathname)
+
+	if (isGuestLinks && isAuth) {
+		return redirect(links.actors.index)
+	}
+
+	if (!isGuestLinks && !isAuth) {
+		return redirect(links.login)
+	}
+
 	return NextResponse.next()
 }
 
